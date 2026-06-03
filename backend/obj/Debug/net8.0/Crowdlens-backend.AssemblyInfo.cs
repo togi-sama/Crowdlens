98a2ef9c3c7279208690ca667750616e56ef7e2c
togi <jeceneligot2004@gmail.com>
2026-06-03 18:51:33 +0800

    .

--- backend/obj/Debug/net8.0/Crowdlens-backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/Crowdlens-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Crowdlens-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de950d8af1edc637af159bc64e73758684567068")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+933bf18718e41132c346febb166388b4f57e5f2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Crowdlens-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Crowdlens-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
